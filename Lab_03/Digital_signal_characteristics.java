7d55b710a44f327ce7a5573ad6c275e02f3be6fe
ElizavetaKasapen <40471098+ElizavetaKasapen@users.noreply.github.com>
2020-10-18 16:18:01 +0300

    Add files via upload

--- Lab_03/Digital_signal_characteristics.java
+++ Lab_03/Digital_signal_characteristics.java
@@ -6,48 +6,29 @@ import java.util.ArrayList;
 public class Digital_signal_characteristics {
 	 public static void main(String[] args) throws IOException
 	 {
-		// int i;
 		 FileInputStream file_in;
-		 FileOutputStream file_out;
 		 try {
-			 try {
-				 file_in = new FileInputStream(args[0]);	//Input open
-			 	} 
-			 catch (FileNotFoundException e) {
-				 System.out.println ("File not found");	//Warning
-				 return;
-			 	}
-			 try {
-				 file_out = new FileOutputStream(args[1]);	//Output open
-			 	}
-			 catch (FileNotFoundException e) {
-				 System.out.println ("File not found"); //Warning
-				 return;
-			 	}
-		 	}
-		 catch (ArrayIndexOutOfBoundsException e) {
-			 System.out.println ("Using: CopyFile default");
-			 return;								//Warning message
-		 	}
+			 file_in = new FileInputStream(args[0]);	//Input open
+		 	} 
+		 catch (FileNotFoundException e) {
+			 System.out.println ("File not found");	//Warning
+			 return;
+		 	} 								
 	       try {
-	    	   byte[] buffer = new byte[file_in.available()];
-	    	   file_in.read(buffer, 0, buffer.length);
+	    	   byte[] buffer = new byte[file_in.available()];//Create an array for data
+	    	   file_in.read(buffer, 0, buffer.length);//Reading from file
 	    	   System.out.println("Dynamic range = "+Calculations_for_signals.Dynamic_range(buffer));
 	    	   System.out.println("Energy = "+Calculations_for_signals.Energy(buffer));
 	    	   System.out.println("Power = "+Calculations_for_signals.Power(buffer));
 	    	   System.out.format("Mean = %.4f\n",Calculations_for_signals.Mean(buffer)); 
 	    	   System.out.format("Dispersion = %.4f\n",Calculations_for_signals.Dispersion(buffer)); 
-	    	   //for(i=0;i<buffer.length;i++)
-	    		//   System.out.println (buffer[i]+"\n"); 
-	    	   //file_out.write(buffer, 0, buffer.length);
 	       }
 	       catch(IOException ex){
 	             
-	            System.out.println(ex.getMessage());
+	            System.out.println("Error!");//Warning
 	        } 
 	       finally{
 	    	   file_in.close();	//Input close
-	    	   file_out.close();	//Output close
 	       }
 	 }
 }
